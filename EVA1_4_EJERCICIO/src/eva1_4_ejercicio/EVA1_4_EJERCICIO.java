4647b206413266daebbac0a4649aa7e38bc3ac0c
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-08-28 22:07:20 -0600

    Add files via upload

---
+++ EVA1_4_EJERCICIO/src/eva1_4_ejercicio/EVA1_4_EJERCICIO.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_ejercicio;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA1_4_EJERCICIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("EMPLEO: ");
+        System.out.print("ADMINISTRADOR DE BASES DE DATOS");
+        System.out.print("SALARIO: ");
+        System.out.print("12000 AL MES");
+        System.out.print("HORAS A LA SEMANA: ");
+        System.out.print(55);
+        System.out.print("REQUIERE INGLES: ");
+        System.out.print(true);
+    }
+    
+}
