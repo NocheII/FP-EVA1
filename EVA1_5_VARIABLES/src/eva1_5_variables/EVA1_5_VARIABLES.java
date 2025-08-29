e60f88ad40eb6992628241ecd01c43dfe05131f2
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-08-28 22:08:05 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Nombre;
+        int Edad;
+        double Salario;
+        Nombre = "Carlos Moreno Noche";
+        Edad = 18;
+        Salario = 200.20;
+        System.out.println(Nombre);
+        System.out.println(Edad);
+        System.out.println(Salario);
+    }
+    
+}
