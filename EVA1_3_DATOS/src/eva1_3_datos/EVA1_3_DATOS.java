392bf9a8f81e6db648ff32e69e9faefdaa22a90f
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-08-28 22:05:54 -0600

    Add files via upload

---
+++ EVA1_3_DATOS/src/eva1_3_datos/EVA1_3_DATOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_datos;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA1_3_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("NOMBRE DEL ESTUDIANTE: ");
+        System.out.println("CARLOS MORENO NOCHE");
+        System.out.print("NUMERO DE CONTROL: ");
+        System.out.println(25550865);
+        System.out.print("EDAD: ");
+        System.out.println(18);
+    }
+    
+}
