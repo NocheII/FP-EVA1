1610ddca85881baf5db93a8dfb49cea5a7db9224
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-08-28 22:02:40 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // "   " --> CADENA DE TEXTO. DATO
+        System.out.println("Carlos Moreno Noche");
+        // NUMEROS ENTEROS 
+        System.out.println(1000);
+        //NUMEROS DECIMALES
+        
+        System.out.println(500.3);
+        //VALORES LOGICOS (VERDADERO O FALSO)
+        System.out.println(true);
+        // CARACTERES INDIVIDUALES
+        System.out.println('c'); //CARACTER
+        System.out.println("c"); // CADENA DE TEXTO
+    }
+    
+};
