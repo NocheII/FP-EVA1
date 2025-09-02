5b1928d4d4fd0ea2c25aa9b82733d3acb8bfb729
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-09-01 21:50:20 -0600

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double fahrenheit,celcius;
+        
+        fahrenheit = 50;
+        
+        celcius = 5*(fahrenheit-32)/9;
+        
+        System.out.print("Los 50 grados fahrenheit a celcius son: ");
+        System.out.println(celcius);
+    }
+    
+}
