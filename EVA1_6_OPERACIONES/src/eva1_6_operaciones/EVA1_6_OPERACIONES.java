89cdd452dcd59530de013e03bac63af4d0fccefc
NocheII <L25550865@chihuahua2.tecnm.mx>
2025-09-01 21:38:11 -0600

    Add files via upload

---
+++ EVA1_6_OPERACIONES/src/eva1_6_operaciones/EVA1_6_OPERACIONES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_operaciones;
+
+/**
+ *
+ * @author Moren
+ */
+public class EVA1_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double p, r, area;
+
+        p = 3.1416;
+
+        r = 10;
+
+        area = p * (r * r);
+        System.out.print("El area de un circulo de radio 10 es de: ");
+        System.out.println(area);
+    }
+    
+}
+
